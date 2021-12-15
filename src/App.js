617bab10594121accbe86d031bad645575dc1a19
Giuseppe Gallo Cassarino <giuseppegallocassarino@MBP-di-Giuseppe.home-life.hub>
2021-12-15 21:43:54 +0100

    feisbrut

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
 import './App.css';
 
 function App() {
+  const name = "Feisbrut";
+  const nav = [
+    { link: "/", label: "Home" },
+    { link: "/messages", label: "Messages" },
+    { link: "/friends", label: "Friends" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Header name={name} links={nav} />
+       <Home />
+      <Footer />
+      
     </div>
   );
 }
